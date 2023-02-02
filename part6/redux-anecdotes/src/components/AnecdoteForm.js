0caa7467e9881ad0186ef9f8e36277dd1fdbd77a
Guting Huang <h.guting77@gmail.com>
2023-02-02 16:36:52 +0200

    Completed exercises 6.14-6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setMsg, removeMsg } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.postAnecdote(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setMsg(`"${content}" added`))
     setTimeout(() => {
       dispatch(removeMsg())
